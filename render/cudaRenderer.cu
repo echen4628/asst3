f3e80ac8d973d5732d13853fe14b19e8e95770d9
echen4628 <echen4628@gmail.com>
2024-11-10 04:19:26 +0000

    correct patch version. Got half of the points

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -14,6 +14,8 @@
 #include "sceneLoader.h"
 #include "util.h"
 
+#include "circleBoxTest.cu_inl"
+
 ////////////////////////////////////////////////////////////////////////////////////////
 // Putting all the cuda kernels here
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -635,37 +637,56 @@ CudaRenderer::advanceAnimation() {
     cudaDeviceSynchronize();
 }
 
-__global__ void kernelRenderPixels() {
-    int row = blockIdx.x*blockDim.x+threadIdx.x;
-    int col = blockIdx.y*blockDim.y+threadIdx.y;
+__global__ void kernelRenderPixels(int patch_x, int patch_y) {
+    // int row = blockIdx.x*blockDim.x+threadIdx.x;
+    // int col = blockIdx.y*blockDim.y+threadIdx.y;
+
+
+    // short screenMinX = max(0, min(minX, imageWidth));
+    short imageWidth = cuConstRendererParams.imageWidth;
+    short imageHeight = cuConstRendererParams.imageHeight;
+
+    float min_row = max(0, min((blockIdx.x*blockDim.x+threadIdx.x)*patch_y, imageHeight));
+    float min_col = max(0, min((blockIdx.y*blockDim.y+threadIdx.y)*patch_x, imageWidth));
+
+    float max_row = max(0, min((blockIdx.x*blockDim.x+threadIdx.x+1)*patch_y, imageHeight));
+    float max_col = max(0, min((blockIdx.y*blockDim.y+threadIdx.y+1)*patch_x, imageWidth));
 
     for (int index=0; index<cuConstRendererParams.numCircles; index++){
+
         int index3 = 3 * index;
 
         // read position and radius
         float3 p = *(float3*)(&cuConstRendererParams.position[index3]);
         float  rad = cuConstRendererParams.radius[index];
+        
+
 
         // compute the bounding box of the circle. The bound is in integer
         // screen coordinates, so it's clamped to the edges of the screen.
-        short imageWidth = cuConstRendererParams.imageWidth;
-        short imageHeight = cuConstRendererParams.imageHeight;
+
         // float circleCenterX = imageWidth*(p.x);
         // float circleCenterY = imageHeight*(p.y);
         // if ((row-circleCenterY)*(row-circleCenterY)+(col-circleCenterX)*(col-circleCenterX) > rad){
         //     continue;
         // }
 
+        // printf("Row: %d, Col %d, circleCenterX: %f, circleCenterY: %f\n", min_row, min_col, circleCenterX, circleCenterY);
+
+        if (0 == circleInBoxConservative(p.x, p.y, rad, min_col/imageWidth, max_col/imageWidth,
+                                max_row/imageHeight, min_row/imageHeight)){
+                                    continue;
+                                }
 
 
-        short minX = static_cast<short>(imageWidth * (p.x - rad));
-        short maxX = static_cast<short>(imageWidth * (p.x + rad)) + 1;
-        short minY = static_cast<short>(imageHeight * (p.y - rad));
-        short maxY = static_cast<short>(imageHeight * (p.y + rad)) + 1;
-        if (!(minX <= col <maxX) || !(minY <= row < maxY)){
-            continue;
-        }
-        // printf("Row: %d, Col %d, circleCenterX: %f, circleCenterY: %f\n", row, col, circleCenterX, circleCenterY);
+        // short minX = static_cast<short>(imageWidth * (p.x - rad));
+        // short maxX = static_cast<short>(imageWidth * (p.x + rad)) + 1;
+        // short minY = static_cast<short>(imageHeight * (p.y - rad));
+        // short maxY = static_cast<short>(imageHeight * (p.y + rad)) + 1;
+        // if (!(minX <= col <maxX) || !(minY <= row < maxY)){
+        //     continue;
+        // }
+        // printf("Row: %d, Col %d, circleCenterX: %f, circleCenterY: %f\n", min_row, min_col, circleCenterX, circleCenterY);
         // printf("should be painting\n");
         // // a bunch of clamps.  Is there a CUDA built-in for this?
         // short screenMinX = (minX > 0) ? ((minX < imageWidth) ? minX : imageWidth) : 0;
@@ -677,9 +698,9 @@ __global__ void kernelRenderPixels() {
         float invHeight = 1.f / imageHeight;
 
         // for all pixels in the bonding box
-        for (int pixelY=row; pixelY<row+1; pixelY++) {
-            float4* imgPtr = (float4*)(&cuConstRendererParams.imageData[4 * (pixelY * imageWidth + col)]);
-            for (int pixelX=col; pixelX<col+1; pixelX++) {
+        for (int pixelY=min_row; pixelY<max_row; pixelY++) {
+            float4* imgPtr = (float4*)(&cuConstRendererParams.imageData[4 * (pixelY * imageWidth + (int) min_col)]);
+            for (int pixelX=min_col; pixelX<max_col; pixelX++) {
                 float2 pixelCenterNorm = make_float2(invWidth * (static_cast<float>(pixelX) + 0.5f),
                                                     invHeight * (static_cast<float>(pixelY) + 0.5f));
                 shadePixel(index, pixelCenterNorm, p, imgPtr);
@@ -687,8 +708,8 @@ __global__ void kernelRenderPixels() {
             }
         }
     }
-
 }
+
 void
 CudaRenderer::render() {
 
@@ -698,11 +719,13 @@ CudaRenderer::render() {
 
     // kernelRenderCircles<<<gridDim, blockDim>>>();
 
+    int patch_x = 4;
+    int patch_y = 4;
     dim3 blockDim(16, 16);
     printf("image height is %d\n", image->height);
-    dim3 gridDim((image->height + blockDim.x - 1) / blockDim.x, 
-                 (image->width + blockDim.y - 1) / blockDim.y);
-    kernelRenderPixels<<<gridDim, blockDim>>>();
+    dim3 gridDim((image->height/patch_x + blockDim.x - 1) / blockDim.x, 
+                 (image->width/patch_y + blockDim.y - 1) / blockDim.y);
+    kernelRenderPixels<<<gridDim, blockDim>>>(patch_x, patch_y);
 
     // first we need a function that can create a list that tells us what pixels needs to be colored
     // daastructure, maybe can be an array of vectors, each vector can have arbitrary length
